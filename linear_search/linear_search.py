35cf82e943e8078c9656f0e0fbce155b07dc1547
pes adam <pesadam15@gmail.com>
2021-09-30 19:59:29 +0200

    opraveny input

--- linear_search/linear_search.py
+++ linear_search/linear_search.py
@@ -1,3 +1,7 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]  
+print(numbers)
+cislo = int(input("zadaj cislo ktore chces najst: "))
+
 def linear_search(list, target):
     # Vracia index kde sa nachadza target ak nie tak nevrati nic
     
@@ -13,8 +17,6 @@ def verify(index):
     else:
         print("Target nebol najdeny :(")
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,]  
-
-result = linear_search(numbers, input("Zadaj cislo: ")) 
+result = linear_search(numbers, cislo) 
 
 verify(result)      
